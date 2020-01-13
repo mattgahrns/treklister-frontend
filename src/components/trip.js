4efceac1c84baa2b3904f89befc2d88536926c7f
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-13 11:31:09 -0800

    Added the before and after forms to the trip page to add list items

--- src/components/trip.js
+++ src/components/trip.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { api } from '../services/api'
+import { Form } from 'semantic-ui-react';
 
 class Trip extends React.Component {
     constructor(){
         super();
         this.state = {
-            trip: null
+            trip: null,
+            beforeItem: '',
+            afterItem: ''
         }
     }
 
@@ -21,6 +24,19 @@ class Trip extends React.Component {
         });
     }
 
+    handleChange = e => {
+        const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
+        this.setState({ fields: newFields });
+    };
+
+    handleBeforeSubmit = e => {
+
+    }
+
+    handleAfterSubmit = e => {
+        
+    }
+
     render(){
         return(
         <>
@@ -28,6 +44,29 @@ class Trip extends React.Component {
                 <>
                     <h1>{this.state.trip.name}</h1>
                     <h5>{this.state.trip.description !== null ? this.state.trip.description : 'No description found.'}</h5>
+                    <h1>Before leaving to my destination:</h1>
+
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Input
+                            label='New Item'
+                            placeholder="Don't forget to..."
+                            name='beforeItem'
+                            onChange={this.handleChange}
+                        />
+                        <Form.Button content='Submit' />
+                    </Form>
+
+                    <h1>Before leaving to go home:</h1>
+
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Input
+                            label='New Item'
+                            placeholder="Don't forget to..."
+                            name='afterItem'
+                            onChange={this.handleChange}
+                        />
+                        <Form.Button content='Submit' />
+                    </Form>
                 </>
             :
                 <p>Loading...</p>
