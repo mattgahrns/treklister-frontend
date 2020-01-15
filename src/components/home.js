17e247489d29095110963b94b80fa1814429cd88
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-14 16:58:19 -0800

    Added a blurb to the home page describing the app

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Home = () => {
 
     return(
+    <>
         <h1>Treklister Home</h1>
+        <p>Treklister is a web application designed
+            with travelers in mind. It lets you create
+            trips which include two dynamic check lists.
+            One list for things you can't forget before
+            leaving from home, and another for before
+            leaving for back home. Since the lists are 
+            dynamic they can be added to, deleted from,
+            and edited on the fly. Your trips are all 
+            saved in our database and can easily be 
+            reused as many times as you like. To get 
+            started signup today using the link above!
+        </p>
+    </>
     )
 }
 
