dc3fd3ac9e26120131dffdd13e3801bd7c33e1b8
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-23 13:24:01 -0800

    Finished the home page

--- src/components/home.js
+++ src/components/home.js
@@ -5,18 +5,31 @@ const Home = () => {
     return(
     <>
         <h1>Treklister Home</h1>
-        <p>Treklister is a web application designed
+        <div id='iframeContainer'>
+        <iframe 
+        width="1000" 
+        height="600" 
+        src="https://www.youtube.com/embed/R-ekRgefOZI" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen
+        align='middle'>
+        </iframe>
+        </div>
+        <br/>
+        <p id='homePageDesc'>Treklister is a web application designed
             with travelers in mind. It lets you create
             trips which include two dynamic check lists.
             One list for things you can't forget before
-            leaving from home, and another for before
-            leaving for back home. Since the lists are 
+            leaving, and a second to make sure you don't leave anything behind. 
+            Since the lists are 
             dynamic they can be added to, deleted from,
             and edited on the fly. Your trips are all 
             saved in our database and can easily be 
             reused as many times as you like. To get 
-            started signup today using the link above!
+            started signup today using the navigation bar above!
         </p>
+        
     </>
     )
 }
